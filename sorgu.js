f9b65218f1ed51b11566a122daf123c8292a5885
karacatokat <kt@tr.net>
2015-05-06 01:10:43 +0300

    Create sorgu.js

---
+++ sorgu.js
@@ -0,0 +1,5 @@
+var kuphesapla = require("./islem.js");
+console.log("Kenari 2 olan kupun hacmi: " + kuphesapla.kuphacmi(2)); 
+//kuphesapla fonksiyonu icinde tanımlı islem.js 
+//altındaki kuphesapla fonksiyonuna 2 degerini 
+//vererek calistirdim.
